c0eba59e9ad08b0e63c39820263c43e995c4a6f2
Marie <muramatsumarie@gmail.com>
2023-05-08 22:20:49 +0900

    advanced done

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -11,23 +11,47 @@ class ViewController: UIViewController {
     
     @IBOutlet var profileImage : UIImageView!
     @IBOutlet var profileComment : UILabel!
+    @IBOutlet var profileLabel : UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        profileImage.isHidden = true
-        profileComment.isHidden = true
+
         
         
         
     }
     
-    @IBAction func tapButton(){
-        profileImage.isHidden = false
-        profileComment.isHidden = false
+    @IBAction func tapButton1(){
+       
+        profileImage.image = UIImage(named:"philImage")
+        profileLabel.text = "名前"
+        profileComment.text = "Iphoneメンターのフィルだよ"
     }
-
+    
+    @IBAction func tapButton2(){
+        
+        profileImage.image = UIImage(named:"trackImage")
+        profileLabel.text = "スポーツ"
+        profileComment.text = "陸上競技が好きで、走り幅跳びが得意"
+        
+    }
+    
+    @IBAction func tapButton3(){
+        profileImage.image = UIImage(named:"appleImage")
+        profileLabel.text = "好きな食べ物"
+        profileComment.text = "りんごが好きで、いつも持ち歩いてる"
+    }
+    
+    
+    @IBAction func tapButton4(){
+        profileImage.image = UIImage(named:"flightImage")
+        profileLabel.text = "趣味"
+        profileComment.text = "飛行機に乗って、空を散歩すること"
+    }
+    
 
 }
 
